44f617248a3ccfcaa163ed28997d0a70a3108c98
Oluwatodimu <todimuisewon@gmail.com>
2022-04-20 03:09:22 +0100

    Deleted to start from scratch

--- ethereum/factory.js
+++
@@ -1,9 +0,0 @@
-import web3 from './web3';
-import CampaignFactory from './build/CampaignFactory.json';
-
-const instance = new web3.eth.Contract(
-  JSON.parse(CampaignFactory.interface),
-  '0x5b72Ad8C86f7e4936a79f8BcDD78AfE52C164eb7'
-);
-
-export default instance;
